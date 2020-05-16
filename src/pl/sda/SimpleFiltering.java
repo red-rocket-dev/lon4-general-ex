afa5319ac3df936f9132bb8ea687817a3ca0eaf0
Jakub <kubplon@gmail.com>
2020-05-16 03:21:31 +0200

    simple filtering ex

---
+++ src/pl/sda/SimpleFiltering.java
@@ -0,0 +1,23 @@
+package pl.sda;
+
+public class SimpleFiltering {
+
+    /**
+     * 1.Stwórz klasę człowiek mającą pola: imię, data urodzenia, płeć (pamiętaj o nadaniu odpowiednich typów i angielskich nazwach):
+     * Stwórz tablicę, która będzie zawierała następujące osoby:
+     * Adam, data urodzenia 01.01.2022
+     * Basia, data urodzenia 01.01.2002
+     * Paweł, data urodzenia 01.01.2003
+     * Kasia, data urodzenia 01.01.2000
+     * Marcin, data urodzenia 01.01.2011
+     * Wypisz:
+     * a) osobno mężczyzn i osobno kobiety
+     * b) osoby pełnoletnie
+     * c) wszystkie osoby, których wiek jest wielokrotnością 10
+     * d) zsumuj wiek wszystkich osób (ile łącznie mają lat)
+     * e) wszystkie osoby mające “si” w imieniu
+     */
+    public static void main(String[] args) {
+
+    }
+}
