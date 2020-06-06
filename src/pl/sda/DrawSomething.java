581c4a1144acd48d8fc9718ee9f23db7fc2f6649
Jakub <kubplon@gmail.com>
2020-06-06 11:05:06 +0200

    drawing solution

--- src/pl/sda/DrawSomething.java
+++ src/pl/sda/DrawSomething.java
@@ -36,6 +36,69 @@ public class DrawSomething {
      * *********
      */
     public static void main(String[] args) {
+        drawRectangleOwnImpl(20, 20, true);
+        //drawSquare(5);
+    }
 
+    public static void drawSquare(int a) {
+        drawRectangleOwnImpl(a, a, false);
+    }
+
+    public static void drawRectangle(int height, int width, boolean isChessBoardFill) {
+        int filler = 0;
+        for (int a = 0; a < width; a++) {
+            System.out.print("*");
+        }
+        if (!isChessBoardFill) {
+            System.out.println();
+            for (int i = 0; i < height - 2; i++) {
+                System.out.print("*");
+                for (int x = 0; x < width - 2; x++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                System.out.println();
+            }
+        } else {
+            System.out.println();
+            for (int i = 0; i < height - 2; i++) {
+                System.out.print("*");
+                for (int x = 0; x < width - 2; x++) {
+                    if (filler % 2 == 0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                    filler++;
+                }
+                System.out.print("*");
+                System.out.println();
+            }
+        }
+        for (int a = 0; a < width; a++) {
+            System.out.print("*");
+        }
+    }
+
+    public static void drawRectangleOwnImpl(int a, int b, boolean isChessBoardFill) {
+        for (int y = 0; y < a; y++) {
+            for (int x = 0; x < b; x++) {
+                boolean isOnBorder = x == 0 || x == b - 1 || y == 0 || y == a - 1;
+                if (isOnBorder) {
+                    System.out.print("*");
+                } else {
+                    if (isChessBoardFill) {
+                        if ((x + y) % 2 == 0) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
