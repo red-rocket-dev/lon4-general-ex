912fb07ea365dbaf5bf0c15c6d3f641f864920b1
Jakub <kubplon@gmail.com>
2020-06-06 12:03:10 +0200

    too much too little solution

--- src/pl/sda/TooMuchTooLittle.java
+++ src/pl/sda/TooMuchTooLittle.java
@@ -1,5 +1,8 @@
 package pl.sda;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TooMuchTooLittle {
     /**
      * Gra w za dużo za mało przebiega następująco:
@@ -10,6 +13,19 @@ public class TooMuchTooLittle {
      * 5. Jeśli podana odpowiedź jest równa wylosowanej to gra wypisuje: "Gratulacje, wygrana!" i kończy grę.
      */
     public static void main(String[] args) {
-
+        Random randomGenerator = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = randomGenerator.nextInt(100) + 1;
+        System.out.println("Witam w grze Za duzo, Za malo\nPodaj Liczbe od 1 do 100");
+        int usersNumber;
+        do {
+            usersNumber = scanner.nextInt();
+            if (usersNumber < randomNumber) {
+                System.out.println("Liczba jest za mala");
+            } else if (usersNumber > randomNumber) {
+                System.out.println("liczba jest za duza");
+            }
+        } while (randomNumber != usersNumber);
+        System.out.println("Wygrales(-as)!!!!:D");
     }
 }
